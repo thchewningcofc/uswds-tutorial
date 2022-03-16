2e97820b81d1fac3d2b85b15fa27ea494299ad0a
Dan O. Williams <daniel.williams@gsa.gov>
2022-03-15 20:13:31 -0700

    Remove all uswds files and code except those in original styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const uswds = require('@uswds/compile');
 
 uswds.settings.version = 3;
-uswds.paths.dist.theme = './_theme';
 uswds.paths.src.projectSass = './assets/css';
+uswds.paths.dist.theme = './assets/css';
+uswds.paths.dist.css = './assets/css';
 
-exports.init = uswds.init;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
 
