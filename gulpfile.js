e3e8f846a31b9e57761b30968f86fc8890684921
Dan O. Williams <daniel.williams@gsa.gov>
2022-03-15 21:22:01 -0700

    Use settings.version = 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 const uswds = require('@uswds/compile');
 
-// uswds.settings.version = 3;
+uswds.settings.version = 2;
 uswds.paths.dist.theme = './_theme';
 uswds.paths.src.projectSass = './assets/css';
 
 exports.init = uswds.init;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
-
